e994580c430f6f0003e9fdc5e2de224b50bb462f
NA <n-aoki@iMac-n-aoki.local>
2016-09-24 19:45:31 +0900

    新規追加

---
+++ site-cookbooks/mysql/attributes/default.rb
@@ -0,0 +1,4 @@
+default['mysql']['user'] = "vagrant"
+default['mysql']['group'] = "vagrant"
+default['mysql']['server_root_password'] = "rootpass"
+default['mysql']['slave_ipaddress'] = "192.168.33.14"
